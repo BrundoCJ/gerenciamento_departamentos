f9be4618c21f7768173cf1955e98f62a9237c16a
BrundoCJ <bconrado04@gmail.com>
2026-03-24 17:14:27 -0300

    adicionando service

--- demo/src/main/java/com/gerenciamento_de_departamentos/demo/service/DepartamentoService.java
+++ demo/src/main/java/com/gerenciamento_de_departamentos/demo/service/DepartamentoService.java
@@ -1,4 +1,38 @@
 package com.gerenciamento_de_departamentos.demo.service;
 
+import com.gerenciamento_de_departamentos.demo.entity.Departamento;
+import com.gerenciamento_de_departamentos.demo.repository.DepartamentoRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class DepartamentoService {
+
+    private final DepartamentoRepository departamentoRepository;
+
+    public DepartamentoService(DepartamentoRepository departamentoRepository) {
+        this.departamentoRepository = departamentoRepository;
+    }
+
+    public Departamento salvar(Departamento departamento) {
+        return departamentoRepository.save(departamento);
+    }
+
+    public List<Departamento> listarTodos() {
+        return departamentoRepository.findAll();
+    }
+
+    public Optional<Departamento> buscarPorId(Long id) {
+        return departamentoRepository.findById(id);
+    }
+
+    public boolean deletar(Long id) {
+        if (departamentoRepository.existsById(id)) {
+            departamentoRepository.deleteById(id);
+            return true;
+        }
+        return false;
+    }
 }
